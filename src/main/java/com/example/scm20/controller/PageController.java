e73daa290f5210aef36369410ec530e978bb8597
Shirisha-kumbar <shirishakshirisha2525@gmail.com>
2025-09-07 13:32:17 +0530

    added some files

--- src/main/java/com/example/scm20/controller/PageController.java
+++ src/main/java/com/example/scm20/controller/PageController.java
@@ -2,7 +2,9 @@ package com.example.scm20.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // Request ko handel karthe hai
 @Controller
@@ -37,7 +39,24 @@ public class PageController {
         return "services"; 
     }
 
+    // contact page handler
+    @GetMapping("/contact")
+    public String contact(){
+        return new String("contact");
+    }
 
+    // Login page handler
+    @GetMapping("/login")
+    public String login(){
+        return new String("login");
+    }
+
+    // Login page handler
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+    
     @RequestMapping("/index")
     public String index() {
         System.out.println("Homepage handler");
