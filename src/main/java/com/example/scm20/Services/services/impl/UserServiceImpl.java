bbc044f5b274d6eb987f455a91884f4671e9d97b
Shirisha-kumbar <shirishakshirisha2525@gmail.com>
2025-09-13 10:08:07 +0530

    from branch 10

---
+++ src/main/java/com/example/scm20/Services/services/impl/UserServiceImpl.java
@@ -0,0 +1,101 @@
+package com.example.scm20.Services.services.impl;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.scm20.Repositories.UserRepo;
+import com.example.scm20.Services.UserService;
+import com.example.scm20.entities.User;
+import com.example.scm20.helpers.ResourceNotFoundException;
+
+@Service
+public class UserServiceImpl  implements UserService{
+
+    @Autowired
+    private UserRepo userRepo;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Override
+    public User saveUser(User user) {
+        String userId = UUID.randomUUID().toString();
+        user.setUserId(userId);
+        // PASSWORD ENCODE 
+        // user.setPassword(userId);
+        // user.setProfilePic()
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(String id) {
+        return userRepo.findById(id);
+
+    }
+
+  @Override
+public Optional<User> updateUser(User user) {
+    User user2 = userRepo.findById(user.getUserId())
+                                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+    user2.setName(user.getName());
+    user2.setEmail(user.getEmail());
+    user2.setPassword(user.getPassword());
+    user2.setPhoneNumber(user.getPhoneNumber());
+    user2.setAbout(user.getAbout());
+    user2.setProfilePic(user.getProfilePic());
+    user2.setEnabled(user.isEnabled());
+    user2.setEmailVerified(user.isEmailVerified());
+    user2.setPhoneVerified(user.isPhoneVerified());
+    user2.setProviders(user.getProviders());
+    user2.setProviderUserId(user.getProviderUserId());
+
+    User save = userRepo.save(user2);
+
+    return Optional.of(userRepo.save(user2));
+}
+
+    @Override
+    public void deleteUser(String id) {
+        User user2 = userRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        userRepo.delete(user2);
+    }
+
+    @Override
+    public boolean isUserExist(String userId) {
+         User user2 = userRepo.findById(userId).orElse(null);
+        return user2 != null ? true : false;
+    }
+
+    @Override
+    public boolean isUserExistByEmail(String email) {
+        User user = userRepo.findByEmail(email).orElse(null);
+        return user != null ? true : false;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepo.findByEmail(email).orElse(null);
+    }
+
+     // UserService.java
+public interface UserService {
+    User getUserByEmail(String email);
+}
+
+// UserServiceImpl.java
+
+}
--- src/main/java/com/example/scm20/controller/PageController.java
+++ src/main/java/com/example/scm20/controller/PageController.java
@@ -1,14 +1,25 @@
 package com.example.scm20.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.scm20.Services.UserService;
+import com.example.scm20.entities.User;
+import com.example.scm20.forms.UserForm;
 
 // Request ko handel karthe hai
 @Controller
 public class PageController {
+
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/home")
     public String home(Model model) {
@@ -17,7 +28,6 @@ public class PageController {
         model.addAttribute("youtubeChannel", "Learn with Shirisha");
         model.addAttribute("github", "https://github.com/Shirishakshirisha");
 
-        
         return "home"; 
     }
 
@@ -53,17 +63,47 @@ public class PageController {
 
     // Login page handler
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model){
+       UserForm userForm = new UserForm();
+    //    u can add the default data in the empty from 
+    //  u can delete this code also and ur name and othe data
+        // userForm.setName("Shirisha K");
+        // userForm.setPassword("Shirisha@000");
+        // userForm.setEmail("shirisha@123gmail.com");
+        // userForm.setAbout("About Shirisha");
+        // userForm.setPhoneNumber("1234567890");
+
+
+         // UserForm--> User
+        User user = User.builder()
+        .name(userForm.getName())
+        .email(userForm.getEmail())
+        .password(userForm.getPassword())
+        .about(userForm.getAbout())
+        .phoneNumber(userForm.getPhoneNumber())
+        .profilePic(
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg")
+        .build();
+        User savedUser = userService.saveUser(user);
+
+        System.out.println("User saved");
+
+        userService.saveUser(user);
+         model.addAttribute("userForm", userForm);
         return "register";
     }
     
     @RequestMapping("/index")
     public String index() {
         System.out.println("Homepage handler");
-       
+        return "index";
+    }
 
-        
-        return "index"; 
+    @RequestMapping(value="/do-register",method=RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm){
+        System.out.println("Processing registration");
+        System.out.println(userForm);
+        return "redirect:/register";
     }
 
 }
